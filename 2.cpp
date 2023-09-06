fceaca333988e4e8341b6fa657a1bdb5929a7dc3
olya <olgastet01032005@gmail.com>
2023-09-06 13:04:32 +0300

    class

---
+++ 2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+    int x;
+    int y;
+
+public:
+    void Init()
+    {
+        x = rand() % 20;
+        y = rand() % 10;
+    }
+
+    void Print()
+    {
+        cout << x << "\t" << y << endl;
+    }
+};
+
+int main()
+{
+}
